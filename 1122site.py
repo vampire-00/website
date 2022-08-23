b7c954ff2284242d38178f7547dfb2fa624b4914
vampire-00 <111702581+vampire-00@users.noreply.github.com>
2022-08-23 16:46:34 +0530

    Update 1122site.py

--- 1122site.py
+++ 1122site.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="My Webpage" , page_icon=":tada:", layout="wide")
 with st.container():
     st.subheader("Hi, I am Rohith :wave:")
-    st.title(" A College Student")
-    st.write("This is the website created by me")
-    st.write("fujfpf")
+    st.title(" I am a College Student")
+    st.write("This website is created by me")
+    st.write("Essay Prompt")
 
